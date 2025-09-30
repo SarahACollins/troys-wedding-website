45cd24cad7af2f81637faeedfd69319755dc88c4
Sarah Collins <scollins@epic.com>
2025-09-30 15:49:31 -0500

    making it reset the window

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import devon from './15693.jpg';
 import './App.css';
 
 function App() {
+  const handleRedirect = () => {
+    window.location.href =
+      'https://nemo.webqa.epicexplorers.info/adhocwidgets/OpenScheduling/adhocs/JBY2/resources/openSchedulingSimple.aspx';
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={troy} className="App-logo" alt="logo" />
         <img src={devon} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://nemo.webqa.epicexplorers.info/adhocwidgets/OpenScheduling/adhocs/JBY2/resources/openSchedulingSimple.aspx"
-        >
+        <button className="App-link" onClick={handleRedirect}>
           Josie's ad hoc stage 1 widget
-        </a>
+        </button>
       </header>
     </div>
   );
