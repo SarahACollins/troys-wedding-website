d19f9571a5410c00b88e2c240058b6d5e8662f80
Sarah Collins <scollins@epic.com>
2025-09-30 16:11:26 -0500

    Add diff links for mobile v web

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import devon from './15693.jpg';
 import './App.css';
 
 function App() {
-  const handleRedirect = () => {
+ const handleRedirect = () => {
     window.top.location.href =
       'https://nemo.webqa.epicexplorers.info/adhocwidgets/OpenScheduling/adhocs/JBY2/resources/openSchedulingSimple.aspx';
   };
 
+  // without top redirect
   return (
     <div className="App">
       <header className="App-header">
+        <div>Congrats Troy and Devon</div>
         <img src={troy} className="App-logo" alt="logo" />
         <img src={devon} className="App-logo" alt="logo" />
         <button className="App-link" onClick={handleRedirect}>
-          Josie's ad hoc stage 1 widget
+          Josie's ad hoc stage 1 widget web link
         </button>
+        <a className="App-link" href="https://nemo.webqa.epicexplorers.info/adhocwidgets/OpenScheduling/adhocs/JBY2/resources/openSchedulingSimple.aspx">
+          Josie's ad hoc stage 1 widget mobile link
+        </a>
       </header>
     </div>
   );
